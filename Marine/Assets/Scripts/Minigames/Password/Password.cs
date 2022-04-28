6fafa371bb0f611029bde355ec51851f3163f3d3
Robin van den Bor <firelonze2002@outlook.com>
2022-04-28 08:58:54 +0200

    added Password Puzzle

--- Marine/Assets/Scripts/Minigames/Password/Password.cs
+++ Marine/Assets/Scripts/Minigames/Password/Password.cs
@@ -9,18 +9,29 @@ public class Password : MonoBehaviour
     [SerializeField] private TextMeshProUGUI usernamefield;
     [SerializeField] private TextMeshProUGUI passwordfield;
     private string correctPassword;
-    private string enteredText;
+    private string enteredPassword;
+    private string[] passwords = new string[] { "password", "wordpass", "number", "derulo", "davinci" };
     private string[] usernames = new string[] {"Henk Dirksen","George Nashid","Leonard Davinci","Rapheal Turtle"};
-    
+    private int puzzleInt;
     // Update is called once per frame
     private void Start()
     {
-        
+        puzzleInt = Random.Range(0, usernames.Length);
+        usernamefield.text = usernames[puzzleInt];
+        correctPassword = passwords[puzzleInt];    
+
+
     }
 
-    private void Update()
+    public void CheckPassword()
     {
-        
+        enteredPassword = passwordfield.text;
+        if (enteredPassword == correctPassword)
+        {
+            //Display Green Checkmark - Image
+
+        }
+
     }
 
 }
