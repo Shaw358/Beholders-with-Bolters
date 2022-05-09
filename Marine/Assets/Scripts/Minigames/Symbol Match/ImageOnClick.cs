299e8a036343a59a99eca98ff036ab871bcc4ef5
Robin van den Bor <firelonze2002@outlook.com>
2022-05-10 01:21:17 +0200

    Fixed Symbolmatching minigame
    scripts still need to be assigned to the prefabs of the symbols but that is all

---
+++ Marine/Assets/Scripts/Minigames/Symbol Match/ImageOnClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ImageOnClick : MonoBehaviour
+{
+    /// <summary>
+    /// Script is made in a singular hour, along with the connections.
+    /// Normally you'd enter the correct or incorrect function in this one or attach it so it's accesable.
+    /// lazy lol
+    /// </summary>
+    [SerializeField] bool correctAnswer;
+    [SerializeField] SymbolMatching manager;
+
+    private void Start()
+    {
+       
+        manager = GameObject.Find("MinigameManager").GetComponent<SymbolMatching>();
+    }
+    private void OnMouseDown()
+    {
+        if (correctAnswer)
+        {
+            manager.CorrectAnswer();
+        }
+        else
+        {
+            manager.IncorrectAnswer();
+        }
+    }
+}
