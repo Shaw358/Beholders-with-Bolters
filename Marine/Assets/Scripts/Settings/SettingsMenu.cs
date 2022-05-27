956436c8e82a722577b73851b2a3c1cbe08bef15
Robin van den Bor <firelonze2002@outlook.com>
2022-05-27 19:24:40 +0200

    Added GameSettings
    Resolution still needs to be finished

---
+++ Marine/Assets/Scripts/Settings/SettingsMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Dropdown resolutionDropDown;
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+       resolutions = Screen.resolutions;
+    }
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
