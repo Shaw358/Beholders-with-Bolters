2ec0384345bd986e3cecfae93b9a92804e6cb822
Robin van den Bor <firelonze2002@outlook.com>
2022-05-28 13:50:23 +0200

    Edited Settings Menu
    Resolution has been completed

--- Marine/Assets/Scripts/Settings/SettingsMenu.cs
+++ Marine/Assets/Scripts/Settings/SettingsMenu.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public Dropdown resolutionDropDown;
+    public TMP_Dropdown resolutionDropDown;
     public AudioMixer audioMixer;
     Resolution[] resolutions;
 
     private void Start()
     {
-       resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
     }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
